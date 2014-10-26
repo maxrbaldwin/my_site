2fff8086a6c5827cbf552e1c2a74a53348d51ff9
maxrbaldwin <maxrbaldwin@outlook.com>
2014-10-26 16:35:52 -0400

    Added templates directory for angular routing. Reformatted functions js file to be modular

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,24 @@
-jQuery(function() {
-    var windowHeight = $(window).height(),
-        slideWidth = $('#topSkip').outerWidth(),
-        slideHideRight = slideWidth * -1;
+var events = {
 
-        console.log(slideHideRight);
-
-    $('#topSkip').css('right', slideHideRight + 'px');
-
-    $(window).scroll(function() {
+    globals:  {
+        windowHeight : $(window).height(),
+        windowWidth : $(window).width()
+    },
+    hiddenNav: function() {
         var y = $(window).scrollTop();
-        if (y > windowHeight) {
-            $('#nav').slideDown(1000);
-            $('#topSkip').animate({
-                'right': '0px'
-            }, 1000)
+        if (y > events.globals.windowHeight) {
+            $('#nav').slideDown(300);
 
         }
-        if (y < windowHeight) {
-            $('#nav').slideUp(1000);
-            $('#topSkip').animate({
-                'right': slideHideRight
-            }, 1000)
+        if (y < events.globals.windowHeight) {
+            $('#nav').slideUp(300);
         }
-    });
-});
+    },
+    tileClick: function() {
+        console.log('clicked');
+    }
+}
+
+$(window).scroll(events.hiddenNav);
+
+$('.tile').click(events.tileClick);
