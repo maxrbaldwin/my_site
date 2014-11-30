ad7ca162d5a928b391a7ed0d2f9c5bacc3e12f20
maxrbaldwin <maxrbaldwin@outlook.com>
2014-11-30 01:11:13 -0500

    Added angular form controller into the app

--- content/themes/max_theme/js/app.js
+++ content/themes/max_theme/js/app.js
@@ -1,3 +1,24 @@
-var app = angular.module('mySiteApp', [])
+var app = angular.module('mySiteApp', []);
 
-.config(function($routeProvider))
+app.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+
+app.controller('formController', function($scope){
+    $scope.validate = function() {
+
+    };
+    var send = function() {
+
+    },
+    botEquation = function() {
+        var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            firstNumber = numbers[Math.floor(Math.random() * numbers.length)],
+            secondNumber = numbers[Math.floor(Math.random() * numbers.length)],
+            statement = firstNumber + ' + ' + secondNumber + ' = ?';
+        return statement;
+    };
+
+    $scope.bot = botEquation();
+});
