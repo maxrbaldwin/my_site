68ead6e4e4ca0fa01fea3b4af79cca75df9bf4e0
maxrbaldwin <maxrbaldwin@outlook.com>
2014-11-30 01:29:22 -0500

    Added factory to generate numbers for validation

--- content/themes/max_theme/js/app.js
+++ content/themes/max_theme/js/app.js
@@ -5,20 +5,30 @@ app.config(function($interpolateProvider) {
     $interpolateProvider.endSymbol(']]');
 });
 
-app.controller('formController', function($scope){
-    $scope.validate = function() {
+app.factory('botFactory', function() {
 
+    return {
+        equation: function() {
+            var generateNumber = function() {
+                var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    number = numbers[Math.floor(Math.random() * numbers.length)];
+                return number;
+            },
+                firstNumber = generateNumber(),
+                secondNumber = generateNumber(),
+                statement = firstNumber + ' + ' + secondNumber + ' = ?';
+            return statement;
+
+        },
+        validate: function(equation) {
+            var parts = equation.split(' ');
+            console.log(parts);
+        }
     };
-    var send = function() {
+});
 
-    },
-    botEquation = function() {
-        var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            firstNumber = numbers[Math.floor(Math.random() * numbers.length)],
-            secondNumber = numbers[Math.floor(Math.random() * numbers.length)],
-            statement = firstNumber + ' + ' + secondNumber + ' = ?';
-        return statement;
-    };
+app.controller('formController', function($scope, botFactory) {
+    $scope.bot = botFactory.equation();
 
-    $scope.bot = botEquation();
+    $scope.eqValidate = botFactory.validate($scope.bot);
 });
