9c4bdfc60c4b60fcff79c968bcff29ebbb8140ef
maxrbaldwin <maxrbaldwin@outlook.com>
2015-02-21 13:02:44 -0500

    Removed all functions and started over. Semantic events object and load templates function added

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,69 +1,18 @@
 var myApp = (function() {
-    var init = function() {
-            myApp.scrolling.focus();
+    var semantic_events = {
+
+    };
+    var template = {
+        get_template: function() {
+
         },
-        scrolling = {
-            focus: function() {
-                var active = $('.focused');
-                $('body', 'html').animate({
-                    scrollTop: active.offset().top
-                }, 500);
-            },
-            scroll: function(self) {
-                // var focusedNow = $('.focused'),
-                //     next = $('.focused').next();
+        load_template: function() {
 
-                // next.addClass('focused');
-                // focusedNow.removeClass('focused');
-
-                // $('body, html').animate({
-                //     scrollTop: next.offset()
-                // }, 500);
-            }
-        },
-        events = {
-            globals: {
-                windowHeight: $(window).height(),
-                windowWidth: $(window).width()
-            },
-            hiddenNav: function() {
-                var y = $(window).scrollTop();
-                if (y > events.globals.windowHeight) {
-                    $('#nav').slideDown(300);
-
-                }
-                if (y < events.globals.windowHeight) {
-                    $('#nav').slideUp(300);
-                }
-            },
-            tileClick: function() {
-                console.log('clicked');
-            },
-            filterClick: function() {
-
-            }
-        },
-        formatting = {
-            publishDate: function(time) {
-                var timeSelector = time.text(),
-                    timeStamp = timeSelector.split(' '),
-                    month = timeStamp[1],
-                    date = timeStamp[2],
-                    year = timeStamp[3],
-                    newDate = month + ' ' + date + ', ' + year;
-
-                time.text(newDate);
-            },
-            content: function(content) {
-                var text = content.text(),
-                    moreContent = text + '...';
-                content.text(moreContent);
-            }
         }
+    };
+
     return {
-        init: init,
-        events: events,
-        format: formatting,
-        scrolling: scrolling
-    }
+        temp: template,
+        s_events: semantic_events
+    };
 })();
