43b5c8a13e1c68668916d1309ee9b5bfbcb773a9
maxrbaldwin <maxrbaldwin@outlook.com>
2015-02-21 13:02:03 -0500

    removed all of my events to start with new structure

--- public/js/events.js
+++ public/js/events.js
@@ -1,16 +1,2 @@
-$(window).scroll(myApp.events.hiddenNav);
-
-$('.tile').click(myApp.events.tileClick);
-
-$(document).ready(function(){
-    var timeStamp = $('#templateContainer .date'),
-        postText = $('#templateContainer .blogText').next();
-    myApp.format.publishDate(timeStamp);
-    myApp.format.content(postText);
-    myApp.init();
-});
-
-// $(document).on('scroll', function(self) {
-//     console.log('scroll');
-//     myApp.scrolling.scroll(self);
-// });
+//Semantic Code
+$('.tabular.menu .item').tab();
