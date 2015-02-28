625bce6b0ffe32f53dac6960fbece085e449a153
maxrbaldwin <maxrbaldwin@outlook.com>
2015-02-28 12:42:36 -0500

    Made / route the index route using routing and jade

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+// Node modules
 var express = require('express'),
-    get = require('./controllers/get.js'),
-    post = require('./controllers/post.js'),
     app = express();
 
+// Controllers
+var get = require('./controllers/get.js'),
+    post = require('./controllers/post.js');
+
+// Routes
+var index = require('./routes/index');
+
+// App configs
+app.set('views', './views');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', index.index);
 
 app.route('/posts')
     .get(function(req, res) {
